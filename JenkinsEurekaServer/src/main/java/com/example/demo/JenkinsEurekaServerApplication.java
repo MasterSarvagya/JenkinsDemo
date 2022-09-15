9968fd818306d554a343e17c1ccf0753d5293fc4
Sarvagya Srivastava <sarvagyasrivastava108@gmail.com>
2022-09-15 15:19:55 +0530

    Eureka Server

--- JenkinsEurekaServer/src/main/java/com/example/demo/JenkinsEurekaServerApplication.java
+++ JenkinsEurekaServer/src/main/java/com/example/demo/JenkinsEurekaServerApplication.java
@@ -6,9 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerMain {
+public class JenkinsEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerMain.class, args);
+		SpringApplication.run(JenkinsEurekaServerApplication.class, args);
 	}
+
 }
